59c216f68d87961fb153b451c3bbdc9dc417bb5d
Norman Munge <normanmunge@gmail.com>
2023-04-24 22:46:59 +0300

    Adds paths to static files

--- welfare/welfare/settings.py
+++ welfare/welfare/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
@@ -124,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL='/static/'
+STATIC_ROOT= BASE_DIR / 'staticfiles'
+STATICFILES_DIR=[]
